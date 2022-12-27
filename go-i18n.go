02adcbee6d7dbf52d7046fdd83b595489122da67
bkzy-wangjp <120726080@qq.com>
2022-12-27 22:11:34 +0800

    优化

--- go-i18n.go
+++ go-i18n.go
@@ -2,6 +2,7 @@ package i18n
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 )
@@ -15,7 +16,7 @@ func Init(filepath string, msgdicts ...map[string]map[string]string) {
 			_i18nDict[k] = v
 		}
 	}
-	if jsondicts, err := GetDictFromJsonFile(filepath); err == nil {
+	if jsondicts, err := getDictFromJsonFile(filepath); err == nil {
 		for k, v := range jsondicts {
 			_i18nDict[k] = v
 		}
@@ -27,12 +28,7 @@ func Init(filepath string, msgdicts ...map[string]map[string]string) {
 //language string:zh-CN,en-US等
 //msgdict map[string]map[string]string:字典
 //输出:string: 消息文本
-func I18n(msgcode, language string, msgdicts ...map[string]map[string]string) string {
-	for _, dict := range msgdicts {
-		for k, v := range dict {
-			_i18nDict[k] = v
-		}
-	}
+func I18n(msgcode, language string) string {
 	ecode, ok := _i18nDict[msgcode]
 	if !ok {
 		return msgcode
@@ -45,13 +41,16 @@ func I18n(msgcode, language string, msgdicts ...map[string]map[string]string) st
 }
 
 //将json字典转换为map字典
-func JsonDictToMap(js string) (dict map[string]map[string]string) {
-	json.Unmarshal([]byte(js), &dict)
-	return
-}
+//func jsonDictToMap(js string) (dict map[string]map[string]string) {
+//	json.Unmarshal([]byte(js), &dict)
+//	return
+//}
 
 //从json文件获取字典数据
-func GetDictFromJsonFile(filepath string) (map[string]map[string]string, error) {
+func getDictFromJsonFile(filepath string) (map[string]map[string]string, error) {
+	if len(filepath) == 0 {
+		return nil, fmt.Errorf("file path is empty")
+	}
 	file, err := os.Open(filepath)
 	if err != nil {
 		return nil, err
--- i18n_test.go
+++ i18n_test.go
@@ -3,13 +3,5 @@ package i18n
 import "testing"
 
 func TestI18n(t *testing.T) {
-	textMap, err := GetDictFromJsonFile("testdictfile.json")
-	if err != nil {
-		t.Error(err)
-	} else {
-		cn := I18n("user_unlogin", "zh-CN", textMap)
-		en := I18n("user_unlogin", "en-US", textMap)
-		t.Log(cn)
-		t.Log(en)
-	}
+
 }
